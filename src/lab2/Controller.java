d763bd8c90cc36e1b060d49cf6aa6945c7485f19
Wihlborg <henrik.vihlborg0017@stud.hkr.se>
2019-04-09 11:06:19 +0200

    Read the fucking manual

--- src/lab2/Controller.java
+++ src/lab2/Controller.java
@@ -1,4 +1,31 @@
 package lab2;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    Button sendButton;
+    @FXML
+    Button getButton;
+    @FXML
+    TextField getField;
+    @FXML
+    TextField sendField;
+
+    Database db = Database.getInstance();
+    @FXML
+    public void sendMove(){
+        String move = sendField.getText();
+        db.sendMove(move);
+        sendField.setText("");
+    }
+
+    @FXML
+    public void getMove(){
+        String move = db.getMove();
+        getField.setText(move);
+    }
 }
--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -1,13 +1,15 @@
 package lab2;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
